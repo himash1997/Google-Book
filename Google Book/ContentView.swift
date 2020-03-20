162457a39eb703133e91e53266637a5167310af3
himash1997 <31865243+himash1997@users.noreply.github.com>
2020-03-20 15:46:02 +0530

    update

--- Google Book/ContentView.swift
+++ Google Book/ContentView.swift
@@ -7,10 +7,17 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+import SwiftyJSON
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView{
+            Home()
+            .navigationBarTitle(Text("Books"))
+        }
+        
     }
 }
 
@@ -18,4 +25,62 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Home : View {
+    
+    @ObservedObject var books = GetData()
+    
+    var body : some View{
+        List(books.data){i in
+            Text(i.id)
+        }
+    }
+}
+
+
+class GetData : ObservableObject{
+    
+    @Published var data = [Book]()
+    
+    init() {
+        
+        let url = "https://www.googleapis.com/books/v1/volumes?q=java"
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: URL(string: url)!) { (data, _, err) in
+            if err != nil{
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            let json = try! JSON(data: data!)
+            let items = json["items"].array!
+            
+            for i in items{
+                
+                let id = i["id"].stringValue
+                let title = i["volumeInfo"]["title"]
+                let authors = i["volumeInfo"]["authors"].array!
+                
+                
+                DispatchQueue.main.async {
+                    self.data.append(Book(id: id, title: "", authors: "", desc: "", imgurl: "", url: ""))
+                }
+                
+            }
+            
+        }.resume()
+    }
+    
+}
+
+struct Book : Identifiable {
+    
+    var id : String
+    var title : String
+    var authors : String
+    var desc : String
+    var imgurl : String
+    var url : String
+    
 }
