122b0e03e4f22c3c09f1990d165bf0bc487d2a5f
himash1997 <31865243+himash1997@users.noreply.github.com>
2020-03-20 08:21:39 +0530

    Initial Commit

---
+++ Google Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Google Book
+//
+//  Created by Himash Nadeeshan on 3/20/20.
+//  Copyright © 2020 Himash Nadeeshan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
